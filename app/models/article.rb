4e86991b34d7cddd90921a9e933b27fbf5df694d
Matt Gauger <matt.gauger@gmail.com>
2013-03-10 14:59:04 -0500

    Article factory test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   has_many :notes
-  validates_presence_of :title, :date_published, :date_ingested
+  validates_presence_of :date_ingested
 
   def original_domain
     URI.parse(original_url).host if original_url
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Article do
+  describe 'factories' do
+    it 'make valid records' do
+      create(:article).should be_valid
+      build(:article).should be_valid
+    end
+  end
 end
