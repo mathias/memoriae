0913b301baa3b80182f9820bd7c94f8925fb20a7
Matt Gauger <matt.gauger@gmail.com>
2013-11-17 22:00:02 -0600

    Smarter decoration decent exposure strategy

--- lib/decent_exposure/draper_strong_parameters_strategy.rb
+++ lib/decent_exposure/draper_strong_parameters_strategy.rb
@@ -1,5 +1,6 @@
 class DraperStrongParametersStrategy < DecentExposure::StrongParametersStrategy
   def resource
-    super.decorate
+    r = super.tap { |r| r.class.send(:include, Draper::Decoratable) unless r.respond_to?(:decorate) }
+    r.decorate
   end
 end
